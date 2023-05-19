c712ca7e3f90061785d61cd06fa626dcef9430c7
Carlos Jiménez <carlos.jimenezp@vass.es>
2023-05-19 20:24:45 +0200

    bottom bar

--- app/src/main/java/com/carlosjimz87/instagramclone/screens/HomeScreen.kt
+++ app/src/main/java/com/carlosjimz87/instagramclone/screens/HomeScreen.kt
@@ -1,19 +1,24 @@
 package com.carlosjimz87.instagramclone.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -26,7 +31,6 @@ import com.carlosjimz87.instagramclone.ui.theme.InstagramCloneTheme
 @Composable
 fun HomeScreen() {
     Scaffold(
-
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.largeTopAppBarColors(
@@ -55,9 +59,42 @@ fun HomeScreen() {
                         painter = painterResource(id = R.drawable.message),
                         contentDescription = null
                     )
-                    Spacer(modifier = Modifier .height(6.dp))
+                    Spacer(modifier = Modifier.height(6.dp))
                 }
             )
+        },
+        bottomBar = {
+            Row(
+                modifier = Modifier
+                    .background(Color.White)
+                    .border(1.dp, Color.White)
+                    .fillMaxWidth()
+                    .padding(0.dp, 10.dp),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Image(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = R.drawable.home), contentDescription = null
+                )
+                Image(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = R.drawable.search), contentDescription = null
+                )
+                Image(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = R.drawable.save), contentDescription = null
+                )
+                Image(
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clip(CircleShape)
+                        .border(
+                            1.dp, Color.Red,
+                            CircleShape
+                        ),
+                    painter = painterResource(id = R.drawable.profile1), contentDescription = null
+                )
+            }
         }
     ) {
 
